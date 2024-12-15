61f3fbf9760eb0065e3a92084e8c8f8b66a81410
wiperi <solidtreepassing@gmail.com>
2024-12-16 00:04:48 +0800

    feat: 37 sudoku

--- src/backtrack/37.解数独.ts
+++ src/backtrack/37.解数独.ts
@@ -1,14 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=37 lang=typescript
+ *
+ * [37] 解数独
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify board in-place instead.
+ */
 function solveSudoku(board) {
-  let index = 0;
   let halt = false;
 
-  backtrack();
+  backtrack(0);
 
   return;
 
-  function backtrack() {
-    let floor = Math.floor;
-    let [r, c] = [floor(index / 9), floor(index % 9)];
+  function backtrack(index) {
+    let [r, c] = [Math.floor(index / 9), Math.floor(index % 9)];
 
     // 是否找到解决方案？
     if (index === 9 * 9) {
@@ -18,9 +26,7 @@ function solveSudoku(board) {
 
     // 剪枝：如果当前位置不是空的，从下一个位置开始
     if (board[r][c] !== '.') {
-      index++;
-      backtrack();
-      index--;
+      backtrack(index + 1);
       return;
     }
 
@@ -29,33 +35,29 @@ function solveSudoku(board) {
       if (!isValid(r, c, val)) continue;
 
       // 更新状态
-      index++;
       board[r][c] = String(val);
 
       // 做选择
-      backtrack();
+      backtrack(index + 1);
 
       // 只需要找到一个解决方案
       if (halt) return;
 
       // 撤销状态
       board[r][c] = '.';
-      index--;
     }
   }
 
   function isValid(r, c, val) {
     for (let i = 0; i < 9; i++) {
       // 判断行是否存在重复
-      if (board[r][i] === val) return false;
+      if (board[r][i] === String(val)) return false;
       // 判断列是否存在重复
-      if (board[i][c] === val) return false;
+      if (board[i][c] === String(val)) return false;
       // 判断 3 x 3 方框是否存在重复
-      if (
-        board[Math.floor(r / 3) * 3 + Math.floor(i / 3)][Math.floor(c / 3) * 3 + (i % 3)] === val
-      ) {
-        return false;
-      }
+      let boxRow = Math.floor(r / 3) * 3 + Math.floor(i / 3);
+      let boxCol = Math.floor(c / 3) * 3 + (i % 3);
+      if (board[boxRow][boxCol] === String(val)) return false;
     }
     return true;
   }
@@ -80,3 +82,5 @@ if (require.main === module) {
     console.log(row.join(' '));
   }
 }
+
+// @lc code=end
