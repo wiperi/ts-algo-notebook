0776d9d956f747c5e1951128b0acad7b0f3fa0d2
wiperi <solidtreepassing@gmail.com>
2024-12-16 00:36:41 +0800

    feat: frontend

--- src/frontEnd/Curring.ts
+++ src/frontEnd/Curring.ts
@@ -1,8 +1,8 @@
 let count = 0;
 
 function sumMaker(length: number) {
-  let nums:number[] = [];
-  
+  let nums: number[] = [];
+
   const sum = (...args: number[]): any => {
     nums.push(...args);
     if (nums.length >= length) {
@@ -12,7 +12,7 @@ function sumMaker(length: number) {
     } else {
       return sum;
     }
-  }
+  };
 
   return sum;
 }
@@ -20,59 +20,14 @@ function sumMaker(length: number) {
 function typeCheckerGenerator(type: string) {
   return (val: any) => {
     return typeof val === type;
-  }
+  };
 }
 
+if (require.main === module) {
+  const sum = sumMaker(3);
 
-// test
-const sum = sumMaker(3);
+  console.log(sum(2)(2));
 
-console.log(sum(2)(2))
-
-const numCheck = typeCheckerGenerator('number');
-console.log(numCheck('234'));
-
-
-function race(promises: unknown[]) {
-  // returned promise will be resolved based on the first resolved given promises
-  return new Promise((resolve, reject) => {
-    promises.forEach(p => Promise.resolve(p).then(resolve, reject));
-  })
+  const numCheck = typeCheckerGenerator('number');
+  console.log(numCheck('hello'));
 }
-function all(promises: unknown[]) {
-  // fulfieed when all given promises fulfieed, if any rejected, then rejected
-  return new Promise((resolve, reject) => {
-
-    const res: any = [];
-    let count = 0;
-    promises.forEach((p, i) => Promise.resolve(p).then(
-      val => {
-        res[i] = val;
-        count++;
-        if (count === promises.length) resolve(res);
-      },
-      reject
-    ))
-  })
-}
-
-const p1 = new Promise((r, rej) => {
-  setTimeout(() => {
-    r(99);
-  }, 1000);
-})
-const p2 = new Promise((r, rej) => {
-  setTimeout(() => {
-    r(99);
-  }, 2000);
-})
-const p3 = new Promise((r, rej) => {
-  setTimeout(() => {
-    r(99);
-  }, 3000);
-})
-all([p1,p2,p3, 66]).then(
-  res => console.log(res),
-  err => console.log(err)
-)
-
---
+++ src/frontEnd/debounce&throttle.ts
@@ -0,0 +1,30 @@
+// debounce.ts
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | null;
+  return function (...args: Parameters<T>) {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
+  };
+}
+
+// throttle.ts
+export function throttle<T extends (...args: any[]) => void>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean;
+  return function (...args: Parameters<T>) {
+    if (inThrottle) return;
+
+    func.apply(this, args);
+    inThrottle = true;
+    setTimeout(() => (inThrottle = false), limit);
+  };
+}
